24832c9e7a29f68da67d7279024cf6ed9f277141
kei-kusanagi <kei_kusanagi@outlook.com>
2021-12-21 18:56:59 -0600

    se agrega el home

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/pythonprojectejemplo.herokuapp.com')
+@app.route("pythonprojectejemplo.herokuapp.com")
 def inicio():
     return render_template("index.html")
 
